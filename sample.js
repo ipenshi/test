983904e0d263c1679195382fadf3c3c0ed0fa605
ipenshi <43715862+ipenshi@users.noreply.github.com>
2020-06-17 23:15:00 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,9 +1,10 @@
 let practice = document.querySelector('div');
+
 practice.addEventListener('click',rename);
 
 function rename(){
     let name = prompt('文章を入力してください。※入力後訂正不可');
     if(name !== null) {
-        practice.textContent = practice.textContent + name;
+        practice.innerHTML += '<br>' + name
     }
 };
